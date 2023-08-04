4870a9cf96bf0a49185a965ec9b40659e297a5a6
Sgnahore <stephaniergnahore@gmail.com>
2023-08-04 13:12:18 +0100

    excercise complete

---
+++ src/hello-world.ts
@@ -0,0 +1,13 @@
+interface Translations {
+  english: string;
+  esperanto: string;
+  hawaiian: string;
+  turkish: string;
+}
+
+export const HelloWorld: Translations = {
+  english: "Hello world!",
+  esperanto: "Saluton mondo!",
+  hawaiian: "Aloha Honua",
+  turkish: "Merhaba Dünya!",
+};
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,25 @@ import express from "express";
 import ponyData from "../data/ponies.json";
 import { seasonOneEpisodes } from "./episodes";
 import { pickRandom } from "./random";
+import { HelloWorld } from "./hello-world";
 
 const app = express();
 const serverStartDate = new Date();
 let serverHitCount = 0;
+let serverHitList: string[] = [];
 
 app.get("/", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   res.send(
     "This is the default path - and it isn't very interesting, sorry. \nTry visiting localhost:4000/creation-time, localhost:4000/current-time"
   );
 });
 
 app.get("/creation-time", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
   res.json({
     message: `The server was started at ${serverStartDate.toTimeString()}`,
     utc: serverStartDate.toUTCString(),
@@ -22,6 +29,9 @@ app.get("/creation-time", (req, res) => {
 });
 
 app.get("/current-time", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   const dateOfRequestHandling = new Date();
 
   res.json({
@@ -32,6 +42,9 @@ app.get("/current-time", (req, res) => {
 });
 
 app.get("/hits", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   serverHitCount += 1;
   res.json({
     note: "We've registered your hit!",
@@ -41,6 +54,9 @@ app.get("/hits", (req, res) => {
 });
 
 app.get("/hits-stealth", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   res.json({
     note: "Oooh, you ninja. We didn't count that hit.",
     currentTotal: serverHitCount,
@@ -49,6 +65,9 @@ app.get("/hits-stealth", (req, res) => {
 });
 
 app.get("/ponies", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   res.json({
     message: "Loaded dummy JSON data:",
     data: ponyData,
@@ -56,14 +75,31 @@ app.get("/ponies", (req, res) => {
   });
 });
 
+// app.get("/ponies/random", (req, res) => {
+
+//   const randomPony = pickRandom(ponyData)
+//   res.json({
+//     message: "Loaded a single random pony:",
+//     data: randomPony,
+//     countedAsHit: false,
+//   });
+// });
+
 app.get("/season-one", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   res.json({
     countedAsHit: false,
     data: seasonOneEpisodes,
+    routes: serverHitList,
   });
 });
 
 app.get("/season-one/random", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
   const randomEpisode = pickRandom(seasonOneEpisodes);
   res.json({
     countedAsHit: false,
@@ -71,8 +107,23 @@ app.get("/season-one/random", (req, res) => {
   });
 });
 
+app.get("/hello-world", (req, res) => {
+  let currentRoute = req.path;
+  serverHitList.push(currentRoute);
+
+  res.json({
+    data: HelloWorld,
+  });
+});
+
+app.get("/server-hit-list", (req, res) => {
+  res.json({
+    routes: serverHitList,
+  });
+});
+
 // using 4000 by convention, but could be changed
-const PORT_NUMBER = 4000;
+const PORT_NUMBER = 5050;
 
 app.listen(PORT_NUMBER, () => {
   console.log(
